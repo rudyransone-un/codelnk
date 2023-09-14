494d9c3927c7cf0cf7e0a50b04926a8755765743
rudyransone-un <rudyransone@gmail.com>
2023-09-15 02:20:32 +0300

    feat: add Header

--- apps/web/src/components/Header.tsx
+++ apps/web/src/components/Header.tsx
@@ -1,20 +1,29 @@
-import { Link } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 
 export function Header() {
+  const isActivePage = ({ isActive }: { isActive: boolean }) =>
+    isActive ? 'underline' : '';
+
   return (
-    <>
-      <div className="w-full h-[40px] shadow-b-sm flex justify-between items-center">
-        <div className="font-sans text-xl">Codelnk</div>
-        <div className="flex items-center space-x-1 px-2">
-          <Link to={'/'}>Home</Link>
-          <Link to={'upload'}>Upload</Link>
-          <Link to={'profile'}>Profile</Link>
-        </div>
-        <div className="flex items-center space-x-1 px-2">
-          <Link to={'register'}>register</Link>
-          <Link to={'login'}>login</Link>
-        </div>
+    <div className="fixed top-0 left-0 px-5 py-5 w-[100%] h-[50px] shadow-b-sm flex justify-between items-center border-b-[1px] border-slate-100">
+      <Link to={'/'} className="font-sans text-xl">
+        Codelnk
+      </Link>
+      <div className="flex items-center space-x-3 px-2">
+        <NavLink to={'/'} className={isActivePage}>
+          Home
+        </NavLink>
+        <NavLink to={'/upload'} className={isActivePage}>
+          Upload
+        </NavLink>
+        <NavLink to={'/profile'} className={isActivePage}>
+          Profile
+        </NavLink>
       </div>
-    </>
+      <div className="flex items-center space-x-3 px-2">
+        <Link to={'/register'}>Sign-Up</Link>
+        <Link to={'/login'}>Sign-In</Link>
+      </div>
+    </div>
   );
 }
